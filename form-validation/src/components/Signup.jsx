2e3f5e5c1dd275435db9f29a813c7441329c21d3
gift56 <efeasiughu@gmail.com>
2022-06-01 16:36:40 +0100

    created name input

--- form-validation/src/components/Signup.jsx
+++ form-validation/src/components/Signup.jsx
@@ -5,6 +5,12 @@ const Signup = () => {
                 <div>
                     <h2 className="title">Create Account</h2>
                 </div>
+                <form action="" className="form-wrapper">
+                    <div className="name">
+                        <label className="label">Full Name</label>
+                        <input type="text" className="input" />
+                    </div>
+                </form>
             </div>
         </div>
     )
