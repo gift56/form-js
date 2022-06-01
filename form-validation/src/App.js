f89713e4e5502ad7725790c109b5f59acf5735ae
gift56 <efeasiughu@gmail.com>
2022-06-01 16:12:58 +0100

    written the css for the form

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -1,18 +1,8 @@
-import avatar from './asset/teach.jpg';
 import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="container mt-3">
-      <div className="row">
-        <div className="col-md-5">
-          <Signup />
-        </div>
-        <div className="col-md-7">
-          <img src={avatar} alt="" className="img-fluid w-100" />
-        </div>
-      </div>
-    </div>
+    <Signup />
   );
 }
 
