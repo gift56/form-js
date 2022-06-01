0e367a5dfd371eeb5e28cc256b7774d9c0f843e1
gift56 <efeasiughu@gmail.com>
2022-06-01 16:55:30 +0100

    use usestate to validate data

--- form-validation/src/components/Signup.jsx
+++ form-validation/src/components/Signup.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 
 const Signup = () => {
-
     const [values, setValue] = useState({
         fullname: "",
         email: "",
         password: ""
     });
 
+    const handleChange = (e) => {
+        setValue({ ...values, [e.target.name]: e.target.value })
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
     }
@@ -21,15 +23,15 @@ const Signup = () => {
                 <form action="" className="form-wrapper">
                     <div className="name">
                         <label className="label">Full Name</label>
-                        <input type="text" className="input" name="fullname" value={values.fullname} />
+                        <input type="text" className="input" name="fullname" value={values.fullname} onChange={handleChange} />
                     </div>
                     <div className="email">
                         <label className="label">Email Address</label>
-                        <input type="email" className="input" name="email"  value={values.email}/>
+                        <input type="email" className="input" name="email" value={values.email} onChange={handleChange} />
                     </div>
                     <div className="password">
                         <label className="label">password</label>
-                        <input type="password" className="input" name="password"  value={values.password}/>
+                        <input type="password" className="input" name="password" value={values.password} onChange={handleChange} />
                     </div>
                     <div>
                         <button className="submit" onClick={handleSubmit}>Sign Up</button>
