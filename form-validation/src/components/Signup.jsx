dbbc5880e2d898c7cede548a58108863b90ce972
gift56 <efeasiughu@gmail.com>
2022-06-01 16:31:53 +0100

    written title heading

--- form-validation/src/components/Signup.jsx
+++ form-validation/src/components/Signup.jsx
@@ -1,7 +1,11 @@
 const Signup = () => {
     return (
         <div className="container">
-
+            <div className="app-wrapper">
+                <div>
+                    <h2 className="title">Create Account</h2>
+                </div>
+            </div>
         </div>
     )
 }
