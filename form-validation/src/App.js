a224f3812785cb7a343edfada64e8aaf1d95abc3
gift56 <efeasiughu@gmail.com>
2022-06-01 11:58:47 +0100

    created formik in app js

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import avatarImg from './asset/illustator.png';
+import Signup from './component/Signup';
 
 function App() {
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-5">
-          SignUp
+          <Signup />
         </div>
         <div className="col-md-7">
-          <img src={avatarImg} alt="" className='img-fluid' />
+          <img src={avatarImg} alt="" className='img-fluid w-100' />
         </div>
       </div>
     </div>
---
+++ form-validation/src/component/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+
+const Signup = () => {
+  return (
+    <Formik>
+      <h1>Sign Up page</h1>
+    </Formik>
+  )
+}
+
+export default Signup
