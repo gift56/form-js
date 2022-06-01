f55a532115efa71d9f4d0fbfc5153d00a287c1f2
gift56 <efeasiughu@gmail.com>
2022-06-01 12:25:24 +0100

    no network available

--- form-validation/src/component/Signup.js
+++ form-validation/src/component/Signup.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 
 const Signup = () => {
-  return (
-    <Formik>
-      <h1>Sign Up page</h1>
-    </Formik>
-  )
+    return (
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+
+            }}
+        >
+            {formik => (
+                <div>
+                    <h1 className="my-4 font-weight-bold-display-4">Sign Up</h1>
+                    {console.log(formik)}
+                    <Form>
+
+                    </Form>
+                </div>
+            )}
+        </Formik >
+    )
 }
 
 export default Signup
