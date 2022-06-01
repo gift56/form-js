d04f44b3c6b1bea3e793b95cc3fc591e85ac5aef
gift56 <efeasiughu@gmail.com>
2022-06-01 17:23:11 +0100

    created new component file called successful

---
+++ form-validation/src/components/Successful.jsx
@@ -0,0 +1,15 @@
+import './../css/Form.css';
+
+const Successful = () => {
+    return (
+        <div className='container'>
+            <div className="app-wrapper">
+                <h1 className="form-success">
+                    Account Created!
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Successful
