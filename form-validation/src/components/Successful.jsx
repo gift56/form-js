a39b4291d2a08d9ee360a4274645eccb1950396e
gift56 <efeasiughu@gmail.com>
2022-06-11 15:08:33 +0100

    faced new errors

--- form-validation/src/components/Successful.jsx
+++ form-validation/src/components/Successful.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './../css/Form.css';
 
 const Successful = () => {
@@ -12,4 +13,4 @@ const Successful = () => {
     )
 }
 
-export default Successful
+export default Successful;
