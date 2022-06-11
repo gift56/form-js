6143b748d5167d4c1821d7a6fd9ee1751c38da7b
gift56 <efeasiughu@gmail.com>
2022-06-11 16:18:46 +0100

    i tried removing

---
+++ form-validation/build/precache-manifest.49c9c46c52e31379286b83f01e71bc5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cadb913bb871bebac058",
+    "url": "/static/css/main.fcaa64d7.chunk.css"
+  },
+  {
+    "revision": "cadb913bb871bebac058",
+    "url": "/static/js/main.cadb913b.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "71861ff4c4964173f3f1",
+    "url": "/static/js/2.71861ff4.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "aa3926d99cf79b302dde9e33dbe76e77",
+    "url": "/index.html"
+  }
+];
